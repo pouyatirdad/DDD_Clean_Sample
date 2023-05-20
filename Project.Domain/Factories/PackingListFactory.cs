3c11df2583f76777f9245f8ff75af5856c350810
pouya tirdad <pouyatirdad70@gmail.com>
2023-05-20 22:36:50 +0330

    factory

---
+++ Project.Domain/Factories/PackingListFactory.cs
@@ -0,0 +1,24 @@
+﻿using Project.Domain.Consts;
+using Project.Domain.Entities;
+using Project.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Factories
+{
+	public class PackingListFactory : IPackingListFactory
+	{
+		public PackingList Create(PackingListId id, PackingListName name, Localization localization)
+		{
+			throw new NotImplementedException();
+		}
+
+		public PackingList CreateWithDefaultItems(PackingListId id, PackingListName name, TravelDays days, Gender gender, Temperature temperature, Localization localization)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
