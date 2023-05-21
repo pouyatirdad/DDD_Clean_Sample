8ec10b3e17cd4789c0b910469b5adce6d6beba07
pouya tirdad <pouyatirdad70@gmail.com>
2023-05-21 20:41:20 +0330

    add policy and malepolicy

---
+++ Project.Domain/Policies/PolicyData.cs
@@ -0,0 +1,12 @@
+﻿using Project.Domain.Consts;
+using Project.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Policies
+{
+	public record PolicyData(TravelDays Days,Gender Gender,Temperature Temperature);
+}
