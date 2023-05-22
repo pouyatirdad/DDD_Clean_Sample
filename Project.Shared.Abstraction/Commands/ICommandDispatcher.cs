ce6870cd26c967fd6e5e9d3304af97f0a20cf919
pouya tirdad <pouyatirdad70@gmail.com>
2023-05-22 23:00:09 +0330

    cqrs

---
+++ Project.Shared.Abstraction/Commands/ICommandDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Shared.Abstraction.Commands
+{
+	public interface ICommandDispatcher
+	{
+		Task DispatchAsync<TCommand>(TCommand command)where TCommand : class,ICommand;
+	}
+}
---
+++ Project.Shared.Abstraction/Commands/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Shared.Abstraction.Commands
+{
+	public interface ICommandHandler<TCommand> where TCommand :class, ICommand
+	{
+		Task HandlerAsync(TCommand command);
+	}
+}
