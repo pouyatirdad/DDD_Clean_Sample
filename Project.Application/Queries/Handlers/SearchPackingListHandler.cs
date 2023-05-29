120891aee5b45b7fe6b10267fa975c7c9df85750
pouya tirdad <pouyatirdad70@gmail.com>
2023-05-29 21:56:01 +0330

    add searchquery in queries and application layer is done

---
+++ Project.Application/Queries/Handlers/SearchPackingListHandler.cs
@@ -0,0 +1,19 @@
+﻿using Project.Application.DTO;
+using Project.Application.Services;
+using Project.Shared.Abstraction.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Application.Queries.Handlers
+{
+	internal class SearchPackingListHandler : IQueryHandler<SearchPackingList, IEnumerable<PackingListDTO>>
+	{
+		public Task<IEnumerable<PackingListDTO>> HandleAsync(SearchPackingList query)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Project.Application/Queries/SearchPackingList.cs
@@ -0,0 +1,15 @@
+﻿using Project.Application.DTO;
+using Project.Shared.Abstraction.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Application.Queries
+{
+	internal class SearchPackingList:IQuery<IEnumerable<PackingListDTO>>
+	{
+        public string SearchText { get; set; }
+    }
+}
