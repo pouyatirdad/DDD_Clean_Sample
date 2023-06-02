d73a159a3f8326767c4efe2697ef02144bcaf015
pouya tirdad <pouyatirdad70@gmail.com>
2023-06-02 18:52:01 +0330

    add dbcontext

---
+++ Project.Infrastructure/Contexts/ReadDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Infrastructure.Ef.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Infrastructure.Contexts
+{
+	internal sealed class ReadDbContext : DbContext
+	{
+		public DbSet<PackingListReadModel> PackingLists { get; set; }
+
+		public ReadDbContext(DbContextOptions<ReadDbContext> options) : base(options)
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.HasDefaultSchema("packing");
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
