c16617ca04a157118428c525a74e4c92459fc52a
pouya tirdad <pouyatirdad70@gmail.com>
2023-06-02 18:46:29 +0330

    add readmodel

---
+++ Project.Infrastructure/Ef/Models/LocalizationReadModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Infrastructure.Ef.Models
+{
+	internal class LocalizationReadModel
+	{
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Project.Infrastructure/Ef/Models/PackingListReadModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Project.Infrastructure.Ef.Models
+{
+	internal class PackingListReadModel
+	{
+        public Guid Id { get; set; }
+        public int Version { get; set; }
+        public string Name { get; set; }
+        public LocalizationReadModel Localization { get; set; }
+        public ICollection<PackingItemReadModel> items { get; set; }
+    }
+}
