20b71af0495045125bc5b31ace36aea3de88feae
pouya tirdad <pouyatirdad70@gmail.com>
2023-06-09 19:29:28 +0330

    add repository in infrastructure

---
+++ Project.Infrastructure/Ef/Queries/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using Project.Infrastructure.Ef.Models;
+using Project.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Infrastructure.Ef.Queries
+{
+    internal static class Extensions
+    {
+        public static PackingListDTO AsDTO(this PackingListReadModel readModel) => new()
+        {
+            Id = readModel.Id,
+            Name = readModel.Name,
+            Localization = new LocalizationDto
+            {
+                City = readModel.Localization?.City,
+                Country = readModel.Localization?.Country,
+            },
+            items = readModel.items?.Select(x => new PackingItemDTO
+            {
+                IsPacked = x.IsPacked,
+                Name = x.Name,
+                Quantity = x.Quantity
+            })
+        };
+    }
+}
