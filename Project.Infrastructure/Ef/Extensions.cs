77ed108c073adaaa477795bef1340e090ad11c61
pouya tirdad <pouyatirdad70@gmail.com>
2023-06-10 22:11:08 +0330

    postgresPackingListReadService

--- Project.Infrastructure/Ef/Extensions.cs
+++ Project.Infrastructure/Ef/Extensions.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Project.Application.Services;
+using Project.Domain.Repositories;
 using Project.Infrastructure.Ef.Contexts;
 using Project.Infrastructure.Ef.Options;
+using Project.Infrastructure.Ef.Repositories;
+using Project.Infrastructure.Ef.Services;
 using Project.Shared.Options;
 using System;
 using System.Collections.Generic;
@@ -16,6 +20,10 @@ namespace Project.Infrastructure.Ef
 	{
 		public static IServiceCollection AddPostgres(this IServiceCollection services,IConfiguration configuration)
 		{
+			services.AddScoped<IpackingListRepository,PostgresPackingListRepository>();
+			services.AddScoped<IPackingListReadService,PostgresPackingListReadService>();
+
+
 			var options = configuration.GetOptions<PostgresOptions>("Postgres");
 
 			services.AddDbContext<ReadDbContext>(
---
+++ Project.Infrastructure/Ef/Services/PostgresPackingListReadService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Application.Services;
+using Project.Infrastructure.Ef.Contexts;
+using Project.Infrastructure.Ef.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Infrastructure.Ef.Services
+{
+	internal class PostgresPackingListReadService : IPackingListReadService
+	{
+		private readonly DbSet<PackingListReadModel> _packingLists;
+
+		public PostgresPackingListReadService(ReadDbContext context)
+		{
+			_packingLists = context.PackingLists;
+		}
+
+		public Task<bool> ExistsByNameAsync(string name)
+		{
+			return _packingLists.AnyAsync(x=>x.Name == name);
+		}
+	}
+}
